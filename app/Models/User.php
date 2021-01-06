fb28a58f34e23c6082227a71712d5b1333a3a73f
Benedikt Franke <benedikt@franke.tech>
2021-01-07 00:24:46 +0100

    Update to Lighthouse 5 and Laravel 8

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -29,6 +30,7 @@ use Illuminate\Notifications\Notifiable;
  */
 class User extends Authenticatable
 {
+    use HasFactory;
     use Notifiable;
 
     /**
