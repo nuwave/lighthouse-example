0866ea40324387fe67a8d181e9865a2f31ca0087
spawnia <benedikt@franke.tech>
2021-05-11 21:16:37 +0200

    Delete unnecessary files

--- config/app.php
+++ config/app.php
@@ -168,9 +168,6 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
 
     ],
 
--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['graphql'],
 
     'allowed_methods' => ['*'],
 
